6c2b91fe8d4ed2b3fb6849fa1c8001a8965c57bf
NMT81 <tuongnguyen81@outlook.com>
2026-08-28 19:27:17 +0700

    update writer.sh

---
+++ finder-app/writer.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ $# -ne 2 ]; then
+    echo "missing arguments"
+    exit 1
+elif [ -d $1 ]; then
+    echo "argument 1 must be a file"
+    exit 1
+else
+    touch $1 2>/dev/null
+    if [ $? -ne 0 ]; then
+        echo "can't create $1"
+        exit 1
+    fi
+    echo $2 > $1
+    exit 0
+fi
