2e7b4066f5498d402c2cad37b320b900fbc02098
NMT81 <tuongnguyen81@outlook.com>
2026-08-28 20:33:29 +0700

    fix writer.sh fail on hierarchy directory

--- finder-app/writer.sh
+++ finder-app/writer.sh
@@ -7,6 +7,7 @@ elif [ -d $1 ]; then
     echo "argument 1 must be a file"
     exit 1
 else
+    mkdir -p $(dirname $1) 2>/dev/null
     touch $1 2>/dev/null
     if [ $? -ne 0 ]; then
         echo "can't create $1"
